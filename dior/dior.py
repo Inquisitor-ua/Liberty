cf4bba0cc578690662a4ec6bf108db3c96b5d970
Inquisitor-ua <egor.master.2015@gmail.com>
2024-04-11 18:53:14 +0300

    data

--- dior/dior.py
+++ dior/dior.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+from jopa import ins
+
 def pars(url):
     response = requests.get(url)
     html = BeautifulSoup(response.text, "lxml")
     tovars = html.find_all('div', class_='product-tile-wrapper')
     for tovar in tovars:
         name = tovar.find_all("div", class_="product-tile__name u-text-bodycopy u-margin-bottom--xs")
+        opis = ...
+        ins(name, opis)
 
 def main():
     pars(r"https://www.dior.com/ru_ru/beauty/%D1%81%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%B0%D0%BA%D0%B8%D1%8F%D0%B6%D0%B0/%D0%B3%D1%83%D0%B1%D1%8B/%D0%B3%D1%83%D0%B1%D0%BD%D1%8B%D0%B5-%D0%BF%D0%BE%D0%BC%D0%B0%D0%B4%D1%8B")
--- dior/jopa.py
+++ dior/jopa.py
@@ -1,2 +1,7 @@
 import sqlite3
 db = sqlite3.connect("dior/bomba.db")
+c = db.cursor()
+
+def ins():
+    c.execute("INSERT INTO ...")
+    #Добавить код
