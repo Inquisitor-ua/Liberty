bac703813ac7a42e9baf5118b5df2ce18fba0c06
lil-Liberty <evgeniya.alexeevna.legkostup@gmail.com>
2024-04-18 17:35:21 +0200

    git

--- dior/dior.py
+++ dior/dior.py
@@ -16,11 +16,11 @@ def pars(url):
         print(tovar)
         name = tovar.find("div", class_="product-tile__name u-text-bodycopy u-margin-bottom--xs").text.strip('\n')
         opis = tovar.find("div", class_="product-tile__short-description u-margin-bottom--xs").text.strip('\n')
-        ottenki = tovar.find("div", class_="swatchable-colors__count")
-        image = tovar.find("div", class_="image-container position-relative js-image-container") 
+        ottenki = tovar.find("div", class_="tile-body-content js-tile-body").find("div", class_="swatchable-colors__count").text
+        #image = tovar.find("div", class_="image-container position-relative js-image-container") 
         img_url = tovar.find('div', class_='product-tile__base-media').find('img').get('src')
         # download(img_url)
-        ins(name, opis, ottenki, image) 
+        ins(name, opis, ottenki, img_url) 
 
 def main():
     pars(r"https://www.dior.com/ru_ru/beauty/%D1%81%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%B0%D0%BA%D0%B8%D1%8F%D0%B6%D0%B0/%D0%B3%D1%83%D0%B1%D1%8B/%D0%B3%D1%83%D0%B1%D0%BD%D1%8B%D0%B5-%D0%BF%D0%BE%D0%BC%D0%B0%D0%B4%D1%8B")
