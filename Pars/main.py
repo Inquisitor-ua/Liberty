b0c9732886d78697ade7d85a393f41ba89b769ec
lil-Liberty <evgeniya.alexeevna.legkostup@gmail.com>
2024-04-25 17:05:38 +0200

    GG

--- Pars/main.py
+++ Pars/main.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'}
 
-url = 'https://scrapingclub.com/exercise/basic_login/'
+url = 'https://quotes.toscrape.com'
 
 work = Session()
 
 response = work.get(url, headers=headers)
 html = BeautifulSoup(response.text, 'lxml')
-token = html.find('div', class_='my-4 mt-4').find('input').get('value')
+token = html.find('a', href='/login')
 
 data = {"name": '123', 'password': '098', 'csrfmiddlewaretoken': token}
 
