a8081869a4d827925e0bc1ab72586dd8b7bf40ac
Inquisitor-ua <egor.master.2015@gmail.com>
2024-04-25 18:15:35 +0300

    post

--- Pars/main.py
+++ Pars/main.py
@@ -3,14 +3,17 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'}
 
-url = 'https://quotes.toscrape.com'
+url = 'https://quotes.toscrape.com/login'
 
 work = Session()
 
+work.get('https://quotes.toscrape.com', headers=headers)
+
 response = work.get(url, headers=headers)
 html = BeautifulSoup(response.text, 'lxml')
-token = html.find('a', href='/login')
+token = html.find('div', class_='container').find('input').get('value')
 
-data = {"name": '123', 'password': '098', 'csrfmiddlewaretoken': token}
+data = {"username": '123', 'password': '098', 'csrf_token': token}
 
 response = work.post(url, headers=headers, data=data, allow_redirects=True)
+print(response.text)
