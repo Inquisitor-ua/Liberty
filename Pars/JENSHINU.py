455a91c5249e20135c0a2aa78d52fb339add34de
lil-Liberty <evgeniya.alexeevna.legkostup@gmail.com>
2024-04-29 20:40:14 +0200

    fdfbd

---
+++ Pars/JENSHINU.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'}
+i = 1
+url = f"https://scrapingclub.com/exercise/list_infinite_scroll/?page={i}"
+
+while i <= 6:
+    if i == 1:
+        url ="https://scrapingclub.com/exercise/list_infinite_scroll/"
+    else:
+        url = f"https://scrapingclub.com/exercise/list_infinite_scroll/?page={i}"
+    i += 1
+    response = requests.get(url, headers=headers)
+    html = BeautifulSoup(response.text, 'lxml')
+    cards = html.find_all("div", class_="p-4")
+    print(cards)
+    for card in cards:
+        name = card.find("h4").text
+        print(name)
+
+
