35df87b5396568f2273c5eda4d835c66ab5634d2
lil-Liberty <evgeniya.alexeevna.legkostup@gmail.com>
2024-05-09 17:45:41 +0200

    cfdh

--- hueta.py
+++ hueta.py
@@ -13,7 +13,12 @@ def hello(message):
 def message(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, "И тебе привет, дружище!")
+    elif message.text.lower() == "помоги брат":
+        bot.send_message(message.chat.id, "чем?")
+    elif message.text.lower() == "совет":
+        bot.send_message(message.chat.id, "каким")
+    elif message.text.lower() == "кто я брат?":
+        bot.send_message(message.chat.id, "ты брат")
     else:
-        bot.send_message(message.chat.id, "твая мои не понимать")
-    
+         bot.send_message(message.chat.id, "не понимаю брат")
 bot.polling(none_stop=True)
