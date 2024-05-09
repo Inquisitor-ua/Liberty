b26cee4b8f9e73d40ca5b780c9fdc1a9c71a54bd
Inquisitor-ua <egor.master.2015@gmail.com>
2024-05-09 18:52:46 +0300

    markup

--- hueta.py
+++ hueta.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+from telebot import types
 
 TOKEN = config.BOT_TOKEN
 
@@ -7,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start", "home"])
 def hello(message):
-    bot.reply_to(message, "Привет, малиш")
+    bot.reply_to(message, "Привет, малиш", reply_markup=markup)
 
 @bot.message_handler(content_types=["text"])
 def message(message):
@@ -20,5 +21,13 @@ def message(message):
     elif message.text.lower() == "кто я брат?":
         bot.send_message(message.chat.id, "ты брат")
     else:
-         bot.send_message(message.chat.id, "не понимаю брат")
+        bot.send_message(message.chat.id, "не понимаю брат")
+         
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+item1 = types.KeyboardButton("Привет")
+item2 = types.KeyboardButton("Помоги брат")
+item3 = types.KeyboardButton("Совет")
+item4 = types.KeyboardButton("Кто я брат?")
+markup.add(item1, item2, item3, item4)
+
 bot.polling(none_stop=True)
