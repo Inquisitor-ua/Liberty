f95f845d1c983174c8ef7016cc93edb4439f94a7
Inquisitor-ua <egor.master.2015@gmail.com>
2024-05-09 18:29:43 +0300

    gitignore

--- hueta.py
+++ hueta.py
@@ -1,13 +1,19 @@
 import telebot
+import config
 
-TOKEN = "6867993743:AAHBg7sQvBj2Va8ZSUZ3YDZkKg8Mkrdj9ac"
+TOKEN = config.BOT_TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=["start", "home"])
 def hello(message):
     bot.reply_to(message, "Привет, малиш")
 
 @bot.message_handler(content_types=["text"])
 def message(message):
-    bot.send_message(message.chat.id, "твая мои не понимать")
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, "И тебе привет, дружище!")
+    else:
+        bot.send_message(message.chat.id, "твая мои не понимать")
+    
 bot.polling(none_stop=True)
