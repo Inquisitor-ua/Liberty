65ad349032b2bffdf6761963a42e055e19b15947
Inquisitor-ua <egor.master.2015@gmail.com>
2024-06-03 22:02:40 +0300

    Наследование

--- baza/OOP/classes.py
+++ baza/OOP/classes.py
@@ -1,16 +1,41 @@
-class Car():
-    def __init__(self, horse_power, color):
-        self.hp = horse_power
+class Vehicle():
+    def __init__(self, model, color):
         self.color = color
-        
+        self.model = model
+    
+    def start(self):
+        print(f"Двигатель {self.model} работает")
+
+class Car(Vehicle):
+    def __init__(self, horse_power, col, model, color):
+        super().__init__(model, color)
+        self.hp = horse_power
+        self.col = col
+                
     def tunning(self, plus):
         self.hp += (5 *plus)
+        
+    def drive(self):
+        print("Поехали")
+    
+class Plane(Vehicle):
+    def __init__(self, height, weapons, model, color):
+        super().__init__(model, color)
+        self.height = height
+        self.weapons = weapons
+        
+    def fly(self):
+        print("Полетели")
 
-bmw = Car(250, 'black')
-audi = Car(230, 'blue')
+bmw = Car(250, 5, 'E39 M', 'black')
+f16 = Plane(10000, True, 'F-16', 'Gray')
 
-print(bmw.hp)
-plus = int(input("Введите число: "))
-bmw.tunning(plus)
-print(bmw.hp)
-print(audi.hp)
+print(bmw.col)
+print(bmw.color)
+print(f16.weapons)
+print(f16.color)
+
+bmw.start()
+f16.start()
+bmw.drive()
+f16.fly()
